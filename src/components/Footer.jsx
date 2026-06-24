c3d4b0fe9d4548f78dbd25743162afc7e424cf36
Kristina <kristina.lysakk@gmail.com>
2026-06-24 16:52:47 +0200

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        ☀ &nbsp;SOLAR SYSTEM EXPLORER &nbsp;·&nbsp; BUILT WITH REACT + VITE
+        &nbsp;·&nbsp; EDUCATIONAL PROJECT
+      </p>
+    </footer>
+  );
+}
